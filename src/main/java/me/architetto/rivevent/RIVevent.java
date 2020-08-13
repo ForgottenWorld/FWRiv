d98b1b3dfb5a915ed1ee3541a829bd80f6ea6d4e
BuildTools <unconfigured@null.spigotmc.org>
2020-08-13 08:26:05 +0200

    update

--- src/main/java/me/architetto/rivevent/RIVevent.java
+++ src/main/java/me/architetto/rivevent/RIVevent.java
@@ -5,12 +5,14 @@ import me.architetto.rivevent.command.GameHandler;
 import me.architetto.rivevent.listener.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.*;
+import java.util.List;
 import java.util.Objects;
 
 public final class RIVevent extends JavaPlugin {
@@ -26,16 +28,14 @@ public final class RIVevent extends JavaPlugin {
 
         loadConfiguration();
 
+        loadCommands();
+
+        loadListener();
+
         loadPreset();
 
-        Objects.requireNonNull(getCommand("rivevent")).setExecutor(new CommandManager());
+        loadRewardItemList();
 
-        getServer().getPluginManager().registerEvents(new LeftclickListener(),this);
-        getServer().getPluginManager().registerEvents(new FoodLevelListener(),this);
-        getServer().getPluginManager().registerEvents(new DeathListener(),this);
-        getServer().getPluginManager().registerEvents(new QuitListener(),this);
-        getServer().getPluginManager().registerEvents(new DamageListener(),this);
-        getServer().getPluginManager().registerEvents(new SpawnListener(),this);
 
 
     }
@@ -45,8 +45,7 @@ public final class RIVevent extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-    public static <T> void save(T obj)
-            throws Exception {
+    public static <T> void save(T obj) throws Exception {
         ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(
                 pathPreset));
         oos.writeObject(obj);
@@ -76,18 +75,17 @@ public final class RIVevent extends JavaPlugin {
         return defaultConfig;
     }
 
-
     public void loadPreset () {
 
         GameHandler global = GameHandler.getInstance();
+
         File presetFile = new File(pathPreset);
 
         World w = Bukkit.getServer().getWorld(Objects.requireNonNull(getConfig().getString("RESPAWN.w")));
 
-        global.respawnLoc = new Location(w, getConfig().getInt("RESPAWN.x"), getConfig().getInt("RESPAWN.y"),
+        global.endEventRespawnLocation = new Location(w, getConfig().getInt("RESPAWN.x"), getConfig().getInt("RESPAWN.y"),
                 getConfig().getInt("RESPAWN.z"));
 
-        global.experimentalMode = getConfig().getBoolean("EXPERIMENTAL_MODE");
 
         try{
             if (!presetFile.createNewFile ()) {
@@ -99,6 +97,53 @@ public final class RIVevent extends JavaPlugin {
         }
 
     }
-    
+
+    public void loadCommands() {
+
+        Objects.requireNonNull(getCommand("rivevent")).setExecutor(new CommandManager());
+
+    }
+
+    public void loadListener() {
+
+        getServer().getPluginManager().registerEvents(new LeftclickListener(),this);
+        getServer().getPluginManager().registerEvents(new FoodLevelListener(),this);
+        getServer().getPluginManager().registerEvents(new DeathListener(),this);
+        getServer().getPluginManager().registerEvents(new QuitListener(),this);
+        getServer().getPluginManager().registerEvents(new DamageListener(),this);
+        getServer().getPluginManager().registerEvents(new SpawnListener(),this);
+        getServer().getPluginManager().registerEvents(new ProjectileHitListener(),this);
+
+
+    }
+
+    public void loadRewardItemList() {
+
+        GameHandler global = GameHandler.getInstance();
+        FileConfiguration config = getDefaultConfig();
+
+        List<String> MaterialName = config.getStringList("LIST_ITEMS_REWARD");
+
+        if (!MaterialName.isEmpty()){
+            for(String name : MaterialName){
+
+                String [] parts = name.split(",");
+
+                if (Material.getMaterial(parts[0]) != null) {
+                    global.itemsListWeight.put(Material.getMaterial(parts[0]), Integer.parseInt(parts[1]));
+                    global.itemsListMaxAmount.put(Material.getMaterial(parts[0]),Integer.parseInt(parts[2]));
+                }
+
+            }
+        }
+
+        global.totalWeight = global.itemsListWeight.values()
+                .stream()
+                .mapToInt(Integer::valueOf)
+                .sum();
+    }
+
+
+
 
 }
