c0ae8d67bbc7167d45ff1d2c9b89d29ef7a59ca4
BuildTools <unconfigured@null.spigotmc.org>
2020-08-20 16:29:18 +0200

    minor changes

--- src/main/java/me/architetto/rivevent/listener/FoodLevelListener.java
+++ src/main/java/me/architetto/rivevent/listener/FoodLevelListener.java
@@ -1,5 +1,6 @@
 package me.architetto.rivevent.listener;
 
+import me.architetto.rivevent.RIVevent;
 import me.architetto.rivevent.command.GameHandler;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,6 +9,8 @@ import org.bukkit.event.entity.FoodLevelChangeEvent;
 
 
 public class FoodLevelListener implements Listener{
+    int minFoodLevel = RIVevent.getDefaultConfig().getInt("MIN_FOOD_LEVEL");
+    int resetFoodLevel = RIVevent.getDefaultConfig().getInt("RESET_FOOD_LEVEL");
     GameHandler global = GameHandler.getInstance();
 
     @EventHandler
@@ -17,8 +20,8 @@ public class FoodLevelListener implements Listener{
         if (global.playerJoined.contains(entity.getUniqueId())
                 || global.playerSpectate.contains(entity.getUniqueId()) ) {
 
-            if (entity.isOnline()  && entity.getFoodLevel()<=10) {
-                event.setFoodLevel(18); //se scende sotto i 10 lo setta a 18 (per non dare l'effeto regen)
+            if (entity.isOnline()  && entity.getFoodLevel() <= minFoodLevel) {
+                event.setFoodLevel(resetFoodLevel);
 
             }
         }
