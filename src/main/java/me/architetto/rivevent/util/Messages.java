55aa9750b1a6fd3a84dbace88f875cdbe7659cc3
Vincenzo D'Agostino <66284365+ugofosforo@users.noreply.github.com>
2020-08-24 15:43:56 +0200

    new messages

--- src/main/java/me/architetto/rivevent/util/Messages.java
+++ src/main/java/me/architetto/rivevent/util/Messages.java
@@ -5,10 +5,12 @@ import org.bukkit.ChatColor;
 public class Messages{
 
     public static final String NO_CONSOLE = "Comando non eseguibile in console.";
-    public static final String NO_PERM = "Non hai i permessi necessari.";
+    public static final String NO_PERM = "Non hai i permessi necessari per eseguire questo comando.";
+    public static final String NO_PRESET_NAME = "Non hai inserito il nome del preset.";
     public static final String NO_PARAM = "Non hai inserito correttamente i parametri.";
     public static final String NO_PRESET = "Nessun preset con questo nome. Usa '/rivevent list' per la lista dei preset presenti.";
     public static final String NO_EVENT_JOINED = "Non stai partecipando a nessun evento!";
+    public static final String NO_PLAYER_JOINED = "Non ci sono abbastanza partecipanti per iniziare l'evento!";
 
     public static final String OK_INIT = "Evento inizializzato...";
     public static final String OK_JOIN = "Sei stato inserito nella lista dei partecipanti.";
@@ -17,10 +19,11 @@ public class Messages{
     public static final String OK_LEAVE = "Grazie per aver partecipato all'evento!";
     public static final String OK_PRESET = "Il preset è stato correttamente inserito.";
 
+    public static final String ERR_INSERT_PRESET = "Si e' presentato un errore nell'inserimento del preset. Ripetere l'operazione.";
     public static final String ERR_EVENT = "Un evento e' già in corso.";
     public static final String ERR_MINIEVENT = "Un mini-evento e' già in corso.";
     public static final String ERR_NO_EVENT = "Nessun evento in corso.";
-    public static final String ERR_PRESET = "Preset già esistente.";
+    public static final String ERR_PRESET = "Esiste gia' un preset con questo nome, utilizzare un nome diverso.";
     public static final String ERR_PRESET2 = "Stai gia' creando un preset ...";
     public static final String ERR_SETUP_DONE = "Setup gia' effettuato.";
     public static final String ERR_SETUP_NOTREADY = "Setup non effettuato o non ancora terminato.";
@@ -52,6 +55,7 @@ public class Messages{
             " - Per partecipare -> /rivevent join " + "\n" +
             " - Per assistere -> /rivevent spectate" + "\n" +
             ChatColor.RED + ChatColor.ITALIC + ChatColor.UNDERLINE + "PS: Partecipando l'inventario verra' perso!";
+    public static final String NEW_EVENT_TITLE = "Partecipa all'evento 'Resta in vetta' !" + ChatColor.AQUA + " /rivevent join";
 
     public static final String ANTI_CAMPER_MSG = "I superni si accaniscono su di te perche' sei ultimo!";
 
