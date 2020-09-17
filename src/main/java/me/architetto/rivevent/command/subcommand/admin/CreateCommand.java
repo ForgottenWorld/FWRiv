e03eff20040cee0b9c8075c387fbf9e65857badc
BuildTools <unconfigured@null.spigotmc.org>
2020-09-17 23:36:28 +0200

    refactor

--- src/main/java/me/architetto/rivevent/command/subcommand/admin/CreateCommand.java
+++ src/main/java/me/architetto/rivevent/command/subcommand/admin/CreateCommand.java
@@ -29,36 +29,38 @@ public class CreateCommand extends SubCommand{
     GameHandler global = GameHandler.getInstance();
 
     @Override
-    public void perform(Player player, String[] args){
+    public void perform(Player sender, String[] args){
 
-        if (!player.hasPermission("rivevent.create")) {
-            player.sendMessage(ChatMessages.RED(Messages.NO_PERM));
+        if (!sender.hasPermission("rivevent.create")) {
+            sender.sendMessage(ChatMessages.RED(Messages.NO_PERM));
             return;
         }
 
         if (args.length != 2) {
             if (args.length < 2)
-                player.sendMessage(ChatMessages.RED(Messages.NO_PRESET_NAME));
+                sender.sendMessage(ChatMessages.RED(Messages.NO_PRESET_NAME));
             else
-                player.sendMessage(ChatMessages.RED(Messages.NO_PARAM));
+                sender.sendMessage(ChatMessages.RED(Messages.NO_PARAM));
             return;
         }
 
-        if (global.riveventPreset.containsKey(args[1])) {
-            player.sendMessage(ChatMessages.RED(Messages.ERR_PRESET));
+        if (global.riveventPreset.containsKey(args[1].toLowerCase())) {
+            sender.sendMessage(ChatMessages.RED(Messages.ERR_PRESET));
             return;
         }
 
-        if (global.listenerActivator.containsKey(player.getUniqueId())) {
-            player.sendMessage(ChatMessages.RED(Messages.ERR_PRESET2));
+        if (global.listenerActivator.containsKey(sender.getUniqueId())) {
+            sender.sendMessage(ChatMessages.RED(Messages.ERR_PRESET2));
             return;
         }
 
-        global.riveventPreset.put(args[1], new HashMap<>());
-        global.listenerActivator.put(player.getUniqueId(), args[1]);
 
-        player.sendMessage(ChatMessages.GREEN("Right-click block to insert position  ..."));
-        player.sendMessage(ChatMessages.PosMessage("1/6", RightClickListener.Step.SPAWN1));
+        global.riveventPreset.put(args[1].toLowerCase(), new HashMap<>());
+
+        global.listenerActivator.put(sender.getUniqueId(), args[1].toLowerCase());
+
+        sender.sendMessage(ChatMessages.GREEN(Messages.USE_RIGHT_CLICK));
+        sender.sendMessage(ChatMessages.PosMessage("1/6", RightClickListener.Step.SPAWN1));
 
     }
 }
