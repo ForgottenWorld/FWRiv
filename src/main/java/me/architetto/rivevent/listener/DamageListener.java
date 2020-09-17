d94e2ae3ea5cf0138bd24b172d333b1d98f53132
BuildTools <unconfigured@null.spigotmc.org>
2020-09-17 23:04:20 +0200

    removed boogeymanMiniGame code

--- src/main/java/me/architetto/rivevent/listener/DamageListener.java
+++ src/main/java/me/architetto/rivevent/listener/DamageListener.java
@@ -3,13 +3,12 @@ package me.architetto.rivevent.listener;
 import me.architetto.rivevent.RIVevent;
 import me.architetto.rivevent.command.GameHandler;
 import me.architetto.rivevent.util.ChatMessages;
+import me.architetto.rivevent.util.Messages;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class DamageListener implements Listener{
@@ -45,31 +44,12 @@ public class DamageListener implements Listener{
 
                         global.cursedPlayer = damageTaker;
                         damageTaker.playSound(damageTaker.getLocation(), Sound.ENTITY_GHAST_HURT,4,1);
-                        damageTaker.sendMessage("Ti hanno passato la maledizione! Colpisci qualcuno per sbarazzartene ...");
+                        damageTaker.sendMessage(ChatMessages.GOLD(Messages.CURSE_TRANSFER_EVENT));
                         damager.sendMessage("Hai passato la maledizione a " + damageTaker.getDisplayName() + ". Stagli lontano!");
                     }
                 }
 
             }
-
-            if (global.boogeymanEventFlag && global.playerJoined.contains(damager.getUniqueId())
-                    && event.getEntity() instanceof Player) {
-
-                Player damageTaker = ((Player) event.getEntity()).getPlayer();
-                if (damageTaker == global.boogeymanPlayer) {
-
-                    global.boogeymanEventFlag = false;
-
-                    global.boogeymanPlayer.sendMessage(ChatMessages.AQUA("Ti hanno colpito! Torna nel buco da dove sei venuto!"));
-                    global.boogeymanPlayer.damage(30);
-
-                    damager.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 3600, 2));
-                    damager.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 3600, 2));
-
-                }
-
-            }
-
         }
     }
 
