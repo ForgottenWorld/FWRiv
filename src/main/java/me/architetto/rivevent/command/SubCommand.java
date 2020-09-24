78e2b23e44243e7cff344467fdf1644f7b0e4623
Vincenzo D'Agostino <66284365+ugofosforo@users.noreply.github.com>
2020-09-24 22:18:35 +0200

    added TabComplete

--- src/main/java/me/architetto/rivevent/command/SubCommand.java
+++ src/main/java/me/architetto/rivevent/command/SubCommand.java
@@ -2,18 +2,23 @@ package me.architetto.rivevent.command;
 
 import org.bukkit.entity.Player;
 
+import java.util.List;
+
 public abstract class SubCommand {
 
-    //name of the command
+
     public abstract String getName();
 
     
     public abstract String getDescription();
 
-    //How to use command ex. /rivevent create <preset_name>
+
     public abstract String getSyntax();
 
-    //code for the subcommand
+
     public abstract void perform(Player sender, String[] args);
+
+
+    public abstract List<String> getSubcommandArguments(Player player, String args[]);
 
 }
