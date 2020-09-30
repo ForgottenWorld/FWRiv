2595fbe03351a55d7995b2d9e648a6fa2b1e1f39
Vincenzo D'Agostino <66284365+ugofosforo@users.noreply.github.com>
2020-09-30 06:07:18 +0200

    return if !global.setupStartFlag

--- src/main/java/me/architetto/rivevent/listener/ItemDropListener.java
+++ src/main/java/me/architetto/rivevent/listener/ItemDropListener.java
@@ -1,6 +1,7 @@
 package me.architetto.rivevent.listener;
 
 import me.architetto.rivevent.command.GameHandler;
+import me.architetto.rivevent.util.ChatMessages;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
@@ -12,11 +13,15 @@ public class ItemDropListener implements Listener{
     @EventHandler
     public void onItemDrop(PlayerDropItemEvent event){
 
+        if (global.presetSummon.isEmpty() || !global.setupStartFlag)
+            return;
+
         if (global.playerJoined.contains(event.getPlayer().getUniqueId())) {
             event.getItemDrop().remove();
         }
 
         if (global.playerSpectate.contains(event.getPlayer().getUniqueId())) {
+            event.getPlayer().sendMessage(ChatMessages.RED("Non puoi droppare items in questo momento!"));
             event.setCancelled(true);
         }
 
