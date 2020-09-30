b99d7f1d5db060f62ecf48c86d1a16ab70cf1f5b
Vincenzo D'Agostino <66284365+ugofosforo@users.noreply.github.com>
2020-09-30 15:23:53 +0200

    more info as output

--- src/main/java/me/architetto/rivevent/command/subcommand/superuser/EventInfoCommand.java
+++ src/main/java/me/architetto/rivevent/command/subcommand/superuser/EventInfoCommand.java
@@ -4,12 +4,16 @@ import me.architetto.rivevent.command.GameHandler;
 import me.architetto.rivevent.command.SubCommand;
 import me.architetto.rivevent.util.ChatMessages;
 import me.architetto.rivevent.util.Messages;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
-public class EventInfo extends SubCommand{
+public class EventInfoCommand extends SubCommand{
     @Override
     public String getName(){
         return "eventinfo";
@@ -43,10 +47,43 @@ public class EventInfo extends SubCommand{
         sender.sendTitle("", ChatColor.RED + "Partecipanti : " + ChatColor.RESET + global.playerJoined.size()
                + "  ||  " + ChatColor.AQUA + "Spettatori : " + ChatColor.RESET + global.playerSpectate.size(),0,100,0);
 
+        sender.sendMessage(ChatMessages.GREEN("\n====== PARTECIPANTI EVENTO RIV ======\n" + ChatColor.GREEN
+                + "GIOCATORI : " + ChatColor.RESET + getJoinedPlayerList() + ChatColor.AQUA + "\nSPETTATORI : "
+                + ChatColor.RESET + getSpectatorPlayerList()) + "\n==================================");
+
     }
 
     @Override
     public List<String> getSubcommandArguments(Player player, String[] args){
         return null;
     }
+
+    public List<String> getJoinedPlayerList() {
+
+        if (global.playerJoined.isEmpty())
+            return null;
+
+        List<String> playerNameList = new ArrayList<>();
+
+        for (UUID u : global.playerJoined) {
+            playerNameList.add(Objects.requireNonNull(Bukkit.getPlayer(u)).getName());
+        }
+
+        return playerNameList;
+    }
+
+    public List<String> getSpectatorPlayerList() {
+
+        if (global.playerSpectate.isEmpty())
+            return null;
+
+        List<String> playerNameList = new ArrayList<>();
+
+        for (UUID u : global.playerSpectate) {
+            playerNameList.add(Objects.requireNonNull(Bukkit.getPlayer(u)).getName());
+        }
+
+        return playerNameList;
+    }
+
 }
