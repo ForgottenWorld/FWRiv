e80d471e7428d9fe3938c97af8f193bc92f46d3f
BuildTools <unconfigured@null.spigotmc.org>
2020-10-20 23:53:58 +0200

    update

--- src/main/java/me/architetto/rivevent/listener/event/DeathListener.java
+++ src/main/java/me/architetto/rivevent/listener/event/DeathListener.java
@@ -1,16 +1,23 @@
 package me.architetto.rivevent.listener.event;
 
+import me.architetto.rivevent.RIVevent;
 import me.architetto.rivevent.event.EventService;
 import org.bukkit.GameMode;
+import org.bukkit.Particle;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityResurrectEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
 
 public class DeathListener implements Listener{
-
     EventService eventService = EventService.getInstance();
 
-    @EventHandler
+
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerDeath(PlayerDeathEvent event) {
 
         if (!eventService.isRunning())
@@ -18,12 +25,40 @@ public class DeathListener implements Listener{
 
         if (eventService.getParticipantsPlayers().contains(event.getEntity().getUniqueId())) {
 
-            event.getEntity().getInventory().clear();
-            event.getDrops().clear();
             event.setCancelled(true);
+            event.getEntity().getInventory().clear();
             event.getEntity().setGameMode(GameMode.SPECTATOR);
 
             eventService.removePartecipant(event.getEntity().getUniqueId());
+
+        }
+
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onTotemEvent(EntityResurrectEvent event) {
+
+        if (event.isCancelled())
+            return;
+
+        if (!eventService.isRunning())
+            return;
+
+        if (eventService.getParticipantsPlayers().contains(event.getEntity().getUniqueId())) {
+
+            new BukkitRunnable() {
+
+                @Override
+                public void run(){
+
+
+                    Player player = (Player) event.getEntity();
+                    player.getWorld().createExplosion(eventService.getSummonedArena().getTower(),2,false,false);
+                    player.getWorld().spawnParticle(Particle.EXPLOSION_LARGE,player.getLocation(),2);
+                    player.teleport(eventService.getSummonedArena().getTower());
+
+                }
+            }.runTaskLater(RIVevent.plugin,5);
 
         }
 
