59f28c0dc1b3b893eb2e3d5bdea85a094deb01a2
BuildTools <unconfigured@null.spigotmc.org>
2020-10-20 23:54:19 +0200

    permission update

--- src/main/java/me/architetto/rivevent/command/admin/DeleteCommand.java
+++ src/main/java/me/architetto/rivevent/command/admin/DeleteCommand.java
@@ -5,6 +5,7 @@ import me.architetto.rivevent.arena.ArenaManager;
 import me.architetto.rivevent.command.SubCommand;
 import me.architetto.rivevent.event.EventService;
 import me.architetto.rivevent.util.ChatFormatter;
+import me.architetto.rivevent.util.Messages;
 import org.bukkit.entity.Player;
 
 import java.util.List;
@@ -30,20 +31,20 @@ public class DeleteCommand extends SubCommand{
     @Override
     public void perform(Player sender, String[] args) {
 
-        if (!sender.hasPermission("rivevent.delete")) {
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Permission Error: you do not have permission to use that command"));
+        if (!sender.hasPermission("rivevent.admin")) {
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_PERMISSION));
             return;
         }
 
         if (args.length < 2) {
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Syntax Error: insert arena name"));
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_ARENA_CMD_SYNTAX));
             return;
         }
 
         EventService eventService = EventService.getInstance();
 
         if (eventService.isRunning()) {
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Error: an event is running."));
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_EVENT_RUNNING));
             return;
         }
 
@@ -54,9 +55,9 @@ public class DeleteCommand extends SubCommand{
 
         if(preset.isPresent()) {
             presetService.removeArena(presetName);
-            sender.sendMessage(ChatFormatter.formatSuccessMessage(presetName + "preset deleted successfully"));
+            sender.sendMessage(ChatFormatter.formatSuccessMessage(Messages.DELETE_CDM_SUCCESS));
         } else
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Error: there is no preset with this name"));
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_NO_ARENA_NAME));
 
     }
 
