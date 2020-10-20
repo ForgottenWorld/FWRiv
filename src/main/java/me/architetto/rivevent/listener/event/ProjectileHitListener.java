6b0c5f30862dce0841402e26afc0bb7065d25d7d
BuildTools <unconfigured@null.spigotmc.org>
2020-10-20 23:59:19 +0200

    Trident, Fishing rod, snowball code update

--- src/main/java/me/architetto/rivevent/listener/event/ProjectileHitListener.java
+++ src/main/java/me/architetto/rivevent/listener/event/ProjectileHitListener.java
@@ -3,6 +3,9 @@ package me.architetto.rivevent.listener.event;
 
 import me.architetto.rivevent.config.SettingsHandler;
 import me.architetto.rivevent.event.EventService;
+import me.architetto.rivevent.util.ChatFormatter;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -10,6 +13,8 @@ import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
 
 
@@ -27,21 +32,71 @@ public class ProjectileHitListener implements Listener {
         if (!(event.getHitEntity() instanceof Player))
             return;
 
-        Projectile p = event.getEntity();
 
         Player playerHitted = (Player) event.getHitEntity();
 
-        if (p.getType() == EntityType.SNOWBALL
-                && eventService.getParticipantsPlayers().contains(playerHitted.getUniqueId())) {
+        if (!eventService.getParticipantsPlayers().contains(playerHitted.getUniqueId()))
+            return;
 
-            Vector knockbackVector = playerHitted.getLocation().getDirection()
-                    .multiply(settings.snowballKnockbackPower * -1).setY(0.2);
+        Projectile pj = event.getEntity();
+
+        if (pj.getType() == EntityType.SNOWBALL) {
+
+
+
+         // Vector knockbackVector = playerHitted.getLocation().getDirection().multiply(settings.snowballKnockbackPower * -1).setY(0.3);
+
+            Vector knockbackVector = pj.getVelocity();
 
             playerHitted.setVelocity(knockbackVector);
 
             playerHitted.damage(settings.snowballHitDamage);
 
             playerHitted.playSound(playerHitted.getLocation(), Sound.ENTITY_PLAYER_ATTACK_KNOCKBACK,2,1);
+
+            return;
+        }
+
+        //TODO: DA TESTARE
+
+
+        if (pj.getType() == EntityType.FISHING_HOOK) {
+
+            if (pj.getShooter() instanceof Player) {
+
+                Player shooter = (Player) pj.getShooter();
+
+                if (shooter.getInventory().getItemInMainHand().getType() == Material.FISHING_ROD)
+                    shooter.getInventory().setItemInMainHand(null);
+                else if (shooter.getInventory().getItemInOffHand().getType() == Material.FISHING_ROD)
+                    shooter.getInventory().setItemInOffHand(null);
+                else
+                    return; //se non trova una fishing rod non esegue nulla
+
+                Location shooterLoc = shooter.getLocation();
+
+                shooter.teleport(playerHitted.getLocation());
+                shooter.playSound(shooter.getLocation(),Sound.ENTITY_ENDERMAN_TELEPORT,1,1);
+
+                playerHitted.teleport(shooterLoc);
+                playerHitted.playSound(playerHitted.getLocation(),Sound.ENTITY_ENDERMAN_TELEPORT,1,1);
+            }
+            return;
+
+        }
+
+        if (pj.getType() == EntityType.TRIDENT) {
+
+            if (pj.getShooter() instanceof Player) {
+
+                playerHitted.sendMessage(ChatFormatter.formatEventMessage("OUCH!"));
+                playerHitted.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 4));
+                pj.remove();
+
+
+            }
+
+            return;
 
         }
 
