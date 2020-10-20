39c1f7217ccc70feef87192773eb2347f0e87237
BuildTools <unconfigured@null.spigotmc.org>
2020-10-20 23:54:32 +0200

    update

--- src/main/java/me/architetto/rivevent/command/superuser/EventCommand.java
+++ src/main/java/me/architetto/rivevent/command/superuser/EventCommand.java
@@ -3,15 +3,16 @@ package me.architetto.rivevent.command.superuser;
 import me.architetto.rivevent.command.SubCommand;
 import me.architetto.rivevent.event.EventService;
 import me.architetto.rivevent.util.ChatFormatter;
+import me.architetto.rivevent.util.Messages;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.util.List;
 
-public class EventInfoCommand extends SubCommand{
+public class EventCommand extends SubCommand{
     @Override
     public String getName(){
-        return "eventinfo";
+        return "event";
     }
 
     @Override
@@ -27,21 +28,24 @@ public class EventInfoCommand extends SubCommand{
     @Override
     public void perform(Player sender, String[] args){
 
-        if (!sender.hasPermission("rivevent.eventinfo")) {
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Permission Error: you do not have permission to use that command"));
+        if (!sender.hasPermission("rivevent.eventmanager")) {
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_PERMISSION));
             return;
         }
 
         EventService eventService = EventService.getInstance();
 
         if (!eventService.isRunning()){
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Error: no event in progress"));
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_NO_EVENT_RUNNING));
             return;
         }
 
 
-        sender.sendTitle("", ChatColor.RED + "Partecipanti : " + ChatColor.RESET
-                + eventService.getParticipantsPlayers().size() + eventService.getEliminatedPlayers().size(),0,100,0);
+        sender.sendMessage(ChatFormatter.formatSuccessMessage("PLAYERS JOINED : " + ChatColor.YELLOW
+                + eventService.getAllPlayerEvent().size()));
+        sender.sendMessage(ChatFormatter.formatSuccessMessage("PLAYERS LIST : " + ChatColor.YELLOW
+                + eventService.getAllPlayerEvent()));
+        sender.sendMessage(ChatFormatter.formatSuccessMessage("SPECTATORS : WIP"));
 
     }
 
