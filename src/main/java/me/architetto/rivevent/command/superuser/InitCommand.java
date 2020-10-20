baecb91d04a7905bae37a90e38b32281b926beb3
BuildTools <unconfigured@null.spigotmc.org>
2020-10-20 23:36:02 +0200

    clickable textComponent

--- src/main/java/me/architetto/rivevent/command/superuser/InitCommand.java
+++ src/main/java/me/architetto/rivevent/command/superuser/InitCommand.java
@@ -6,6 +6,7 @@ import me.architetto.rivevent.command.SubCommand;
 import me.architetto.rivevent.config.SettingsHandler;
 import me.architetto.rivevent.event.EventService;
 import me.architetto.rivevent.util.ChatFormatter;
+import me.architetto.rivevent.util.Messages;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -37,41 +38,48 @@ public class InitCommand extends SubCommand{
     @Override
     public void perform(Player sender, String[] args){
 
-        if (!sender.hasPermission("rivevent.init")) {
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Permission Error: you do not have permission to use that command"));
+        if (!sender.hasPermission("rivevent.eventmanager")) {
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_PERMISSION));
             return;
         }
 
         if (args.length < 2) {
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Syntax Error: insert preset name"));
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_ARENA_CMD_SYNTAX));
             return;
         }
 
         String presetName = args[1];
 
         ArenaManager arenaManager = ArenaManager.getInstance();
-        SettingsHandler settingsHandler = SettingsHandler.getInstance();
-        Optional<Arena> preset = arenaManager.getArena(presetName);
 
-        if (preset.isPresent()) {
-            EventService eventService = EventService.getInstance();
+        Optional<Arena> arena = arenaManager.getArena(presetName);
 
-            if (settingsHandler.respawnLocation == null) {
-                sender.sendMessage(ChatFormatter.formatErrorMessage("Error: set respawn location with " + ChatColor.GOLD + "/rivevent config setrespawn"));
-                return;
-            }
+        if (!arena.isPresent()) {
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_NO_ARENA_NAME));
+            return;
+        }
 
-            if (eventService.isRunning()) {
-                sender.sendMessage(ChatFormatter.formatErrorMessage("Error: event is already running"));
-                return;
-            }
+        if (SettingsHandler.getInstance().respawnLocation == null) {
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_NO_RESPAWN_LOCATION));
+            return;
+        }
 
-            eventService.initializeEvent(arenaManager.getArenaContainer().get(presetName));
-            sender.sendMessage(ChatFormatter.formatSuccessMessage("Event successfully initialized"));
-            broadcastNewEvent();
-        } else
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Error: this preset doesn't exist"));
+        EventService eventService = EventService.getInstance();
 
+        if (eventService.isRunning()) {
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_EVENT_RUNNING));
+            return;
+        }
+
+        eventService.initializeEvent(arenaManager.getArenaContainer().get(presetName));
+        eventService.setDoorsStatus(false);
+        broadcastNewEvent();
+
+        TextComponent startCMD = new TextComponent(ChatColor.YELLOW + "START");
+        startCMD.setClickEvent( new ClickEvent( ClickEvent.Action.RUN_COMMAND, "/rivevent start") );
+        startCMD.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new Text("Click to start event")));
+        sender.sendMessage(new TextComponent(ChatFormatter.formatSuccessMessage("Evento 'RIV' inizializzato. Click ")),startCMD,
+                new TextComponent(" per startare."));
 
     }
 
@@ -82,18 +90,16 @@ public class InitCommand extends SubCommand{
 
     public void broadcastNewEvent() {
 
-        TextComponent joinClickMessage = new TextComponent(ChatColor.YELLOW + "[ JOIN ]");
+        TextComponent joinClickMessage = new TextComponent(ChatColor.YELLOW + "JOIN");
         joinClickMessage.setClickEvent( new ClickEvent( ClickEvent.Action.RUN_COMMAND, "/rivevent join") );
-        joinClickMessage.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new Text("Click here to join event or use" + ChatColor.YELLOW + " /rivevent join")));
+        joinClickMessage.setHoverEvent( new HoverEvent( HoverEvent.Action.SHOW_TEXT, new Text("Click to join event")));
 
         for (Player p : Bukkit.getOnlinePlayers()) {
 
-            p.sendMessage(new TextComponent(ChatFormatter.formatInitializationMessage("click ")),joinClickMessage,
-                    new TextComponent(" and play with us !" + ChatColor.RED
-                            + ChatColor.ITALIC + " !! your inventory will be deleted !!"));
-
-            p.sendTitle("", ChatColor.AQUA + "RIV event started!"
-                    + ChatColor.YELLOW + " /rivevent join",15,200,15);
+            p.sendMessage(new TextComponent(ChatFormatter.formatInitializationMessage("Click ")),joinClickMessage,
+                    new TextComponent(" per partecipare all'evento 'RIV'"));
+            p.sendMessage(ChatFormatter.formatInitializationMessage(ChatColor.RED + "ATTENZIONE : " + ChatColor.RESET + "Partecipando il tuo inventario verra' cancellato !!"));
+            p.sendTitle("", ChatColor.AQUA + "EVENTO 'RIV'",15,200,15);
 
         }
 
