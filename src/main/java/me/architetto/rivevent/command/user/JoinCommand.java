4a6e62bf6eb2d7fd1976e4a7d56741ec081ff5ed
BuildTools <unconfigured@null.spigotmc.org>
2020-10-20 23:55:42 +0200

    update

--- src/main/java/me/architetto/rivevent/command/user/JoinCommand.java
+++ src/main/java/me/architetto/rivevent/command/user/JoinCommand.java
@@ -3,6 +3,8 @@ package me.architetto.rivevent.command.user;
 import me.architetto.rivevent.command.SubCommand;
 import me.architetto.rivevent.event.EventService;
 import me.architetto.rivevent.util.ChatFormatter;
+import me.architetto.rivevent.util.Messages;
+import org.bukkit.GameMode;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
@@ -28,33 +30,36 @@ public class JoinCommand extends SubCommand {
     @Override
     public void perform(Player sender, String[] args) {
 
-        if (!sender.hasPermission("rivevent.join")) {
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Permission Error: you do not have permission to use that command"));
+        if (!sender.hasPermission("rivevent.user")) {
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_PERMISSION));
             return;
         }
 
         EventService eventService = EventService.getInstance();
 
-        if (eventService.isRunning()) {
-            if (eventService.getParticipantsPlayers().contains(sender.getUniqueId())
-                    || eventService.getEliminatedPlayers().contains(sender.getUniqueId())) {
-                sender.sendMessage(ChatFormatter.formatErrorMessage("Error: you already joined an event"));
+        if (!eventService.isRunning()) {
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_NO_EVENT_RUNNING));
+
+        } else {
+            if (eventService.getAllPlayerEvent().contains(sender.getUniqueId())) {
+                sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_ALREADY_JOINED));
                 return;
             }
+
             if (eventService.isStarted()) {
                 eventService.addEliminated(sender.getUniqueId());
-                sender.teleport(eventService.getSummonedArena().getSpectator());
+                sender.setGameMode(GameMode.SPECTATOR);
+                sender.teleport(eventService.getSummonedArena().getTower());
                 sender.getWorld().playSound(sender.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT,1,1);
-                sender.sendMessage(ChatFormatter.formatSuccessMessage("Event is already in progress. You're been added to spectator"));
-                sender.sendMessage(ChatFormatter.formatSuccessMessage("You are elegible for minievents, so stay ready !"));
+                sender.sendMessage(ChatFormatter.formatSuccessMessage(Messages.JOIN_STARTED_EVENT));
             } else {
                 eventService.addPartecipant(sender.getUniqueId());
-                sender.teleport(eventService.getSummonedArena().getSpectator());
+                eventService.teleportToSpawnPoint(sender);
+                sender.setGameMode(GameMode.SURVIVAL);
                 sender.getWorld().playSound(sender.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT,1,1);
-                sender.sendMessage(ChatFormatter.formatSuccessMessage("You're been added to partecipant. Stay ready!"));
+                sender.sendMessage(ChatFormatter.formatSuccessMessage(Messages.JOIN_EVENT));
             }
-        } else
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Error: no event in progress"));
+        }
 
     }
 
--- src/main/java/me/architetto/rivevent/command/user/LeaveCommand.java
+++ src/main/java/me/architetto/rivevent/command/user/LeaveCommand.java
@@ -4,6 +4,8 @@ import me.architetto.rivevent.command.SubCommand;
 import me.architetto.rivevent.config.SettingsHandler;
 import me.architetto.rivevent.event.EventService;
 import me.architetto.rivevent.util.ChatFormatter;
+import me.architetto.rivevent.util.Messages;
+import org.bukkit.GameMode;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
@@ -28,44 +30,34 @@ public class LeaveCommand extends SubCommand{
 
     @Override
     public void perform(Player sender, String[] args) {
-        if (!sender.hasPermission("rivevent.leave")) {
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Permission Error: you do not have permission to use that command"));
+        if (!sender.hasPermission("rivevent.user")) {
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_PERMISSION));
             return;
         }
 
         EventService eventService = EventService.getInstance();
         SettingsHandler settingsHandler = SettingsHandler.getInstance();
 
-        if (settingsHandler.respawnLocation == null) {
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Error: no respawn location configured!"));
-            return;
-        }
-
         if (!eventService.isRunning()) {
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Error: no event in progress"));
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_NO_EVENT_RUNNING));
             return;
         }
 
-        if (eventService.getParticipantsPlayers().contains(sender.getUniqueId())){
-            if (eventService.isSetupDone())
-                sender.sendMessage(ChatFormatter.formatErrorMessage("You can't leave event now"));
-
-            else {
-                eventService.getParticipantsPlayers().remove(sender.getUniqueId());
-                sender.teleport(settingsHandler.respawnLocation);
-                sender.playSound(sender.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT,1,1);
-                sender.sendMessage(ChatFormatter.formatSuccessMessage("You left the event"));
-
-            }
+        if (eventService.getParticipantsPlayers().contains(sender.getUniqueId())) {
+            eventService.removePartecipant(sender.getUniqueId());
+            sender.teleport(SettingsHandler.getInstance().respawnLocation);
+            sender.playSound(sender.getLocation(),Sound.ENTITY_ENDERMAN_TELEPORT,1,1);
+            sender.sendMessage(ChatFormatter.formatSuccessMessage(Messages.LEAVE_OK));
 
         } else if (eventService.getEliminatedPlayers().contains(sender.getUniqueId())) {
-            eventService.getEliminatedPlayers().remove(sender.getUniqueId());
+            eventService.removeEliminated(sender.getUniqueId());
+            sender.setGameMode(GameMode.SURVIVAL);
             sender.teleport(settingsHandler.respawnLocation);
             sender.playSound(sender.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT,1,1);
-            sender.sendMessage(ChatFormatter.formatSuccessMessage("You left the event"));
+            sender.sendMessage(ChatFormatter.formatSuccessMessage(Messages.LEAVE_OK));
 
         } else
-            sender.sendMessage(ChatFormatter.formatErrorMessage("Error: you are not attending any event"));
+            sender.sendMessage(ChatFormatter.formatErrorMessage(Messages.ERR_NO_EVENT_JOINED));
 
     }
 
