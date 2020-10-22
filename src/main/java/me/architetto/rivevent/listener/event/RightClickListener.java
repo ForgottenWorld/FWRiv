4f4462b82250aaf0506e213a30721eb748d04e93
Vincenzo D'Agostino <66284365+ugofosforo@users.noreply.github.com>
2020-10-22 18:25:12 +0200

    added more effects to targetblock

--- src/main/java/me/architetto/rivevent/listener/event/RightClickListener.java
+++ src/main/java/me/architetto/rivevent/listener/event/RightClickListener.java
@@ -6,6 +6,7 @@ import me.architetto.rivevent.event.EventService;
 import me.architetto.rivevent.util.ChatFormatter;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,6 +16,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.util.Vector;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
@@ -40,23 +42,58 @@ public class RightClickListener implements Listener{
         if (eventService.getParticipantsPlayers().contains(event.getPlayer().getUniqueId())
                 && event.getAction().equals(Action.RIGHT_CLICK_BLOCK)
                 && Objects.requireNonNull(event.getClickedBlock()).getType() == Material.TARGET
-                && Objects.equals(event.getHand(), EquipmentSlot.HAND)){
+                && Objects.equals(event.getHand(), EquipmentSlot.HAND)) {
+
+            player.playSound(player.getLocation(),Sound.BLOCK_STONE_BUTTON_CLICK_ON,1,1);
 
             if (playerCooldown.contains(player.getUniqueId())){
                 player.sendMessage(ChatFormatter.formatErrorMessage("target-block in ricarica..."));
                 return;
             }
 
-            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, new SecureRandom().nextInt(300), 3));
-            player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
-            playerCooldown.add(player.getUniqueId());
+            int targetBlockEffect = new SecureRandom().nextInt(100);
+
+            if (targetBlockEffect < 40) {
+
+                player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION,200, 3));
+                playerCooldown.add(player.getUniqueId());
+
+            } else if (targetBlockEffect > 60) {
+
+                player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 400 , 1));
+                playerCooldown.add(player.getUniqueId());
+
+
+            } else if (targetBlockEffect == 50) {
+
+                player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 400 , 1));
+                playerCooldown.add(player.getUniqueId());
+
+
+            } else {
+
+                Vector knockbackVector = eventService.getSummonedArena().getTower()
+                        .toVector().subtract(player.getLocation().toVector()).multiply(-3);
+
+                player.getWorld().spawnParticle(Particle.EXPLOSION_LARGE,player.getLocation(),2);
+                player.getWorld().playSound(player.getLocation(),Sound.ENTITY_GENERIC_EXPLODE,2,1);
+
+                player.setVelocity(knockbackVector);
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 100 , 1));
+
+
+                playerCooldown.add(player.getUniqueId());
+
+            }
+
+
 
             Bukkit.getScheduler().scheduleSyncDelayedTask(RIVevent.plugin, () -> {
                 playerCooldown.remove(player.getUniqueId());
                 player.sendMessage(ChatFormatter.formatSuccessMessage("target-block reward disponibile!"));
 
 
-            }, 600L);
+            }, settingsHandler.targetBlockCooldown);
 
 
         }
