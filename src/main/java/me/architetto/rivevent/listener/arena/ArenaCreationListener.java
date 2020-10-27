50a04fa42d0c7f585f6e0f91d5cc95db07441408
Vincenzo D'Agostino <66284365+ugofosforo@users.noreply.github.com>
2020-10-27 20:24:54 +0100

    update

--- src/main/java/me/architetto/rivevent/listener/arena/ArenaCreationListener.java
+++ src/main/java/me/architetto/rivevent/listener/arena/ArenaCreationListener.java
@@ -1,25 +1,24 @@
 package me.architetto.rivevent.listener.arena;
 
 import me.architetto.rivevent.arena.ArenaManager;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
 
-import java.io.IOException;
 import java.util.Objects;
 
 public class ArenaCreationListener implements Listener{
 
     @EventHandler
-    public void onRightClickSelection(PlayerInteractEvent event) throws IOException{
+    public void onRightClickSelection(PlayerInteractEvent event) {
         if(!event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
             return;
         }
 
-        if(!event.getHand().equals(EquipmentSlot.HAND)) {
+        if(event.getItem() == null || event.getItem().getType() != Material.STICK) {
             return;
         }
 
