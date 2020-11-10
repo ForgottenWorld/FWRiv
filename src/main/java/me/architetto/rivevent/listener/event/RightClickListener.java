71b5a476b0d315c5a4865a5e6fdacf9cdf168efe
Vincenzo D'Agostino <66284365+ugofosforo@users.noreply.github.com>
2020-11-10 17:16:06 +0100

    fix & new target block rewards

--- src/main/java/me/architetto/rivevent/listener/event/RightClickListener.java
+++ src/main/java/me/architetto/rivevent/listener/event/RightClickListener.java
@@ -14,6 +14,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
@@ -46,8 +47,9 @@ public class RightClickListener implements Listener{
         if (playersManager.isPlayerActive(player.getUniqueId())
                 && material.equals(Material.FIREWORK_ROCKET)) {
 
+            event.setCancelled(true);
             player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
-            player.setVelocity(new Vector(0,1.5,0));
+            player.setVelocity(new Vector(0,1.3,0));
             player.getWorld().playSound(player.getLocation(),Sound.ENTITY_FIREWORK_ROCKET_LAUNCH,3,1);
             return;
 
@@ -73,17 +75,18 @@ public class RightClickListener implements Listener{
 
             Bukkit.getScheduler().scheduleSyncDelayedTask(RIVevent.plugin, () -> {
                 playerCooldown.remove(player.getUniqueId());
+                if (!playersManager.isPlayerActive(player.getUniqueId()))
+                    return;
                 player.sendMessage(ChatFormatter.formatSuccessMessage("target-block reward disponibile!"));
 
             }, settingsHandler.targetBlockCooldown);
 
         }
-
     }
 
     public void targetBlockRewardSystem(Player player, Block targetBlock) {
 
-        int randomInteger = new SecureRandom().nextInt(12);
+        int randomInteger = new SecureRandom().nextInt(16);
 
         switch(randomInteger) {
             case 0:
@@ -94,40 +97,53 @@ public class RightClickListener implements Listener{
                 player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 600 , 1));
                 player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Damage resistance "));
                 break;
-            case 3:
-                player.getInventory().addItem(new ItemStack(Material.ENDER_PEARL, 1));
-                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Ender pearl"));
-                break;
-            case 4:
-                player.getInventory().addItem(new ItemStack(Material.FISHING_ROD, 1));
-                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Fishing rod"));
-                break;
-            case 5:
-                player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 1200 , 1));
-                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Health boost"));
-                break;
-            case 7:
-                player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 600 , 1));
-                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Invisibility"));
-                break;
-            case 8:
-                player.getInventory().addItem(new ItemStack(Material.COOKED_PORKCHOP, 5));
-                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Cooked porkchop"));
-                break;
-            case 9:
-                player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 180 , 1));
-                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Levitation"));
-                break;
-            case 10:
-                player.getInventory().addItem(new ItemStack(Material.SNOWBALL, 5));
-                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Snowball"));
-                break;
-            case 11:
-
+            case 2:
                 Bukkit.getScheduler().scheduleSyncDelayedTask(RIVevent.plugin, () ->
                         player.getInventory().addItem(new ItemStack(Material.FIREWORK_ROCKET, 2)), 10);
 
-                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Firewok rocket"));
+                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Firework rocket"));
+                break;
+            case 4:
+                player.getInventory().addItem(new ItemStack(Material.ENDER_PEARL, 1));
+                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Ender pearl"));
+                break;
+            case 5:
+                player.getInventory().addItem(new ItemStack(Material.FISHING_ROD, 1));
+                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Fishing rod"));
+                break;
+            case 6:
+                player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 1200 , 1));
+                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Health boost"));
+                break;
+            case 8:
+                player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 600 , 1));
+                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Invisibility"));
+                break;
+            case 9:
+                player.getInventory().addItem(new ItemStack(Material.COOKED_PORKCHOP, 5));
+                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Cooked porkchop"));
+                break;
+            case 10:
+                player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 180 , 1));
+                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Levitation"));
+                break;
+            case 11:
+                player.getInventory().addItem(new ItemStack(Material.SNOWBALL, 5));
+                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Snowball"));
+                break;
+            case 13:
+                Bukkit.getScheduler().scheduleSyncDelayedTask(RIVevent.plugin, () ->
+                        player.getInventory().addItem(new ItemStack(Material.FIREWORK_ROCKET, 2)), 10);
+
+                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Firework rocket"));
+                break;
+            case 14:
+                player.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 1200 , 1));
+                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Saturation"));
+                break;
+            case 15:
+                player.getInventory().addItem(randomLingeringPotion());
+                player.sendMessage(ChatFormatter.formatEventMessage("Reward : " + ChatColor.AQUA + "Lingering potion (random)"));
                 break;
             default:
                 Vector knockbackVector = targetBlock.getLocation().toVector().subtract(player.getLocation().toVector()).normalize().multiply(-1.6);
@@ -138,4 +154,44 @@ public class RightClickListener implements Listener{
                 player.setVelocity(knockbackVector);
         }
     }
+
+
+    public ItemStack randomLingeringPotion() {
+        ItemStack p = new ItemStack(Material.LINGERING_POTION, 1);
+        PotionMeta m = (PotionMeta) p.getItemMeta();
+        int randomValue = new SecureRandom().nextInt(6);
+
+        switch(randomValue) {
+            case 0:
+                m.addCustomEffect(new PotionEffect(PotionEffectType.POISON, 200, 0), true);
+                m.setColor(Color.GREEN);
+                break;
+            case 1:
+                m.addCustomEffect(new PotionEffect(PotionEffectType.CONFUSION, 200, 2), true);
+                m.setColor(Color.SILVER);
+                break;
+            case 2:
+                m.addCustomEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 2), true);
+                m.setColor(Color.BLACK);
+                break;
+            case 3:
+                m.addCustomEffect(new PotionEffect(PotionEffectType.HUNGER, 300, 1), true);
+                m.setColor(Color.MAROON);
+                break;
+            case 4:
+                m.addCustomEffect(new PotionEffect(PotionEffectType.REGENERATION, 100, 2), true);
+                m.setColor(Color.RED);
+                break;
+            case 5:
+                m.addCustomEffect(new PotionEffect(PotionEffectType.JUMP, 400, 2), true);
+                m.setColor(Color.AQUA);
+                break;
+        }
+
+        p.setItemMeta(m);
+
+        return p;
+    }
+
+
 }
